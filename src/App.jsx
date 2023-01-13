5ee15cbfb25cc767aa6f8848a34a8048dc7d98b6
Harinathlee <harinathlee2598@gmail.com>
2023-01-13 19:36:45 +0400

    sfsdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-
-import './App.css'
 import About from './components/About/About'
+import Contact from './components/ContactUs/ContactUs'
+import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
 import NavBar from './components/NavBar/NavBar'
+import OurWork from './components/OurWork/OurWork'
+import Reviews from './components/Reviews/Review'
+import WhyUs from './components/WhyUs/WhyUs'
 
 function App() {
 
@@ -11,6 +14,11 @@ function App() {
       <NavBar />
       <Home />
       <About />
+      <OurWork />
+      <WhyUs />
+      <Reviews />
+      <Contact />
+      <Footer />
     </>
   )
 }

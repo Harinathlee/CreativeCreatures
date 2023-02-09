9b7fcbf85599d50dfd539d67506b229cbc3101a2
Harinathlee <harinathlee2598@gmail.com>
2023-02-09 11:29:53 +0400

    added new work

--- src/components/OurWork/SampleEcards.jsx
+++ src/components/OurWork/SampleEcards.jsx
@@ -5,6 +5,12 @@ const SampleEcards = () => {
   return (
     <div className="flex lg:flex-row flex-col justify-center gap-10 p-4 items-center">
       <iframe
+        src="https://drive.google.com/file/d/1vczqNGnyvcPrrYo_9iZu2LecWilTsXm6/preview"
+        className="w-60 h-72 rounded-lg Neomarphism p-1"
+        allow="autoplay"
+        allowFullScreen
+      ></iframe>
+      <iframe
         src="https://drive.google.com/file/d/1Jfyktb-fGqHjFDcJc4i70fxDuttQsqSb/preview"
         className="w-60 h-72 rounded-lg Neomarphism p-1"
         allow="autoplay"
--- src/components/OurWork/SampleVideos.jsx
+++ src/components/OurWork/SampleVideos.jsx
@@ -3,23 +3,27 @@ import "../../index.css";
 
 const SampleVideos = () => {
   return (
+    <div className="flex lg:flex-row flex-col justify-center gap-10 p-4">
+      <iframe
+        src="https://drive.google.com/file/d/1OinnCIR6uxSrOIDkkWPAu7QdvQm6_WjN/preview"
+        className="w-80 h-60 rounded-lg Neomarphism p-1"
+        allow="autoplay"
+        allowFullScreen
+      ></iframe>
+      <iframe
+        src="https://drive.google.com/file/d/1Jmhe-pCoEQ1RtQ9B3vz3QMQrwB-jt1GO/preview"
+        className="w-80 h-60 rounded-lg Neomarphism p-1"
+        allow="autoplay"
+        allowFullScreen
+      ></iframe>
 
-      <div className="flex lg:flex-row flex-col justify-center gap-10 p-4">
-        <iframe
-          src="https://drive.google.com/file/d/1Jmhe-pCoEQ1RtQ9B3vz3QMQrwB-jt1GO/preview"
-          className="w-80 h-60 rounded-lg Neomarphism p-1"
-          allow="autoplay"
-          allowFullScreen
-        ></iframe>
-
-        <iframe
-          src="https://drive.google.com/file/d/1JkByMy_t2_nKiTpRGTLCI67jOkGljxQm/preview"
-          className="w-80 h-60 rounded-lg Neomarphism p-1"
-          allow="autoplay"
-          allowFullScreen
-        ></iframe>
-      </div>
-   
+      <iframe
+        src="https://drive.google.com/file/d/1JkByMy_t2_nKiTpRGTLCI67jOkGljxQm/preview"
+        className="w-80 h-60 rounded-lg Neomarphism p-1"
+        allow="autoplay"
+        allowFullScreen
+      ></iframe>
+    </div>
   );
 };
 
